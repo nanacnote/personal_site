3dad77a22923dc8341567a9d177bc1a7493186e6
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-07-26 20:11:02 +0100

    working

---
+++ my_apps/snake_game/snake.ts
@@ -0,0 +1,163 @@
+/**
+ * TS declaration for Snake module
+ */
+export type TSnake = {
+  params: {
+    ctx: CanvasRenderingContext2D
+    scale: number
+  }
+  props: {
+    x: number
+    y: number
+    key: string
+    addTail: boolean
+    tailLenght: number
+    tail: Array<{ x: number; y: number }>
+  }
+}
+
+/**
+ * Create a new snake object.
+ */
+export default class Snake {
+  /**
+   * @param ctx - An instantiated canvas context
+   * @param scale - Size in pixels to use for the snake
+   * @requires setProps() - call immediately after new instance passing it values for x and y
+   * @example
+   *      const snake = new Snake({
+   *          ctx: document.getElementById('canvas'),
+   *          scale: 5
+   *      })
+   *      snake.setProps({x: 0, y: 0})
+   */
+  constructor(params: TSnake['params']) {
+    this.params.ctx = params.ctx
+    this.params.scale = params.scale
+  }
+
+  /**
+   * smart default / initialiser values for params of the snake class set to an object named param
+   */
+  private params: TSnake['params'] = {
+    ctx: undefined,
+    scale: undefined,
+  }
+  /**
+   *initial properties on snake class set to an object named props, props can only be mutated through setter methods because they are private
+   */
+  private props: TSnake['props'] = {
+    x: undefined,
+    y: undefined,
+    key: undefined,
+    addTail: undefined,
+    tailLenght: 0,
+    tail: [],
+  }
+
+  /**
+   * @public method to expose properties
+   * @returns single current value of property passed as parameter,
+   * of array if parameter passed was array
+   */
+  public getProps(param: 'x' | 'y' | string[] = Object.keys(this.props)) {
+    if (typeof param === 'string') {
+      return this.props[param]
+    } else {
+      const curProps = param.map((e) => this.props[e])
+      return curProps
+    }
+  }
+
+  /**
+   * @public method to set snake class properties
+   */
+  public setProps(param: {
+    x?: number
+    y?: number
+    key?: string
+    addTail?: boolean
+  }) {
+    Object.assign(this.props, param)
+  }
+
+  /**
+   *@private sets the direction of the snake from current or previous input
+   */
+  private getDirection() {
+    switch (this.props.key) {
+      case 'Up':
+        this.props.y -= this.params.scale * 1
+        break
+      case 'Down':
+        this.props.y += this.params.scale * 1
+        break
+      case 'Left':
+        this.props.x -= this.params.scale * 1
+        break
+      case 'Right':
+        this.props.x += this.params.scale * 1
+        break
+      default:
+        break
+    }
+  }
+
+  /**
+   *@private draws the snake head onto the canvas context provided as class param
+   */
+  private growSnake() {
+    if (this.props.addTail) {
+      this.props.tailLenght += 1
+    } 
+    this.props.tail[this.props.tailLenght] = { x: this.props.x, y: this.props.y }
+
+    for (let i = 0; i < this.props.tail.length; i++) {
+        this.props.tail[i - 1] = this.props.tail[i]
+    }
+  }
+  /**
+   *@private grows snakes length when food is eaten and set the tail coordinate for smooth snake motion
+   */
+  private drawHead() {
+    this.params.ctx.strokeStyle = 'white'
+    this.params.ctx.strokeRect(
+      this.props.x,
+      this.props.y,
+      this.params.scale,
+      this.params.scale
+    )
+    this.params.ctx.fillStyle = 'rgb(55, 150, 50)'
+    this.params.ctx.fillRect(
+      this.props.x,
+      this.props.y,
+      this.params.scale,
+      this.params.scale
+    )
+  }
+  /**
+   *@private draws the snake tail onto the canvas context provided as class param
+   */
+  private drawTail() {
+    for (let i = 0; i < this.props.tail.length; i++) {
+    //   this.params.ctx.fillStyle = 'salmon'
+    this.params.ctx.fillStyle = 'rgb(' + Math.floor(255 - 100 * i) + ', ' + Math.floor(255 - 5 * i) + ', 0)';
+      this.params.ctx.fillRect(
+        this.props.tail[i].x,
+        this.props.tail[i].y,
+        this.params.scale,
+        this.params.scale
+      )
+    }
+  }
+
+  /**
+   *@public draws and moves the snake along the x and y plane
+   */
+  public update() {
+    this.growSnake()
+    this.drawTail()
+    this.getDirection()
+    this.drawHead()
+  }
+}
---
+++ my_apps/snake_game/stage.ts
@@ -0,0 +1,173 @@
+import Snake from './snake'
+import Food from './food'
+
+/**
+ * TS declaration for Stage module
+ */
+export type TStage = {
+  params: {
+    canvas: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D,
+    scale: number
+  }
+  props: {
+    xSnake: number
+    ySnake: number
+    xFood: number
+    yFood: number
+    curKey: string
+    prevKey: string
+  }
+}
+export type TInput = {
+  directions: 'Up' | 'Down' | 'Right' | 'Left'
+}
+
+/**
+ * Create a new stage object which will accept assets and create interactive world for them.
+ */
+export default class Stage {
+  /**
+   * @param canvas - The dom canvas element the context is acting on
+   * @param ctx - canvas context currently in use
+   * @param scale - the grid size in pixels
+   * @example
+   *      const snake = new Stage({
+   *          canvas: 'HTMLCanvasElemnt',
+   *          ctx: 'CanvasRenderingContext2D',
+   *          scale: 'number',
+   *      })
+   */
+  constructor(params: TStage['params']) {
+    this.params.canvas = params.canvas
+    this.params.ctx = params.ctx
+    this.params.scale = params.scale
+
+    //instantiate new snake
+    this.instance.snake = new Snake({
+      ctx: this.params.ctx,
+      scale: this.params.scale,
+    })
+    this.instance.snake.setProps({ x: 0, y: 0 })
+
+    //instantiate new food
+    this.instance.food = new Food({
+      canvas: this.params.canvas,
+      ctx: this.params.ctx,
+      scale: this.params.scale,
+    })
+    this.instance.food.setProps({ x: 0, y: 0 })
+  }
+  /**
+   * smart default / initialiser values for params of the snake class set to an object named param
+   */
+  private params: TStage['params'] = {
+    canvas: undefined,
+    ctx: undefined,
+    scale: undefined,
+  }
+  /**
+   * initialises asset require to run game
+   */
+  private instance = {
+    snake: undefined,
+    food: undefined,
+  }
+  //get the current position of x, y and use for subsequent logic
+  public props: TStage['props'] = {
+    xSnake: undefined,
+    ySnake: undefined,
+    xFood: undefined,
+    yFood: undefined,
+    curKey: undefined,
+    prevKey: undefined,
+  }
+
+  /**
+   * @private accepts keyboard input and directs the snake accordingly
+   * @param string
+   */
+  private input(param: TInput['directions']) {
+    //key press regulator
+    const controls = [
+      { key: 'Up', ops: 'Down' },
+      { key: 'Down', ops: 'Up' },
+      { key: 'Right', ops: 'Left' },
+      { key: 'Left', ops: 'Right' },
+    ]
+    //set current direction
+    this.props.curKey = param
+    controls.map((e) => {
+      if (this.props.curKey === e.key && this.props.prevKey !== e.ops) {
+        this.instance.snake.setProps({ key: this.props.curKey })
+        //set previous direction
+        this.props.prevKey = param
+      }
+    })
+  }
+
+  /**
+   * @private
+   * method to monitor assets interaction with boundaries of stage.
+   * Detects when snake hits a boundary and brings it back into canvas on opposite plane
+   */
+  private boundaryInteraction() {
+    //create aliases
+    const canvas = this.params.canvas
+    const snake = this.instance.snake
+
+    //update the stage component with current position of the snake
+    this.props.xSnake = this.instance.snake.getProps('x') as number
+    this.props.ySnake = this.instance.snake.getProps('y') as number
+
+    //determining boundary collision
+    if (this.props.xSnake === canvas.width) {
+      snake.setProps({ x: 0 })
+    }
+    if (this.props.xSnake < 0) {
+      snake.setProps({ x: canvas.width })
+    }
+    if (this.props.ySnake === canvas.height) {
+      snake.setProps({ y: 0 })
+    }
+    if (this.props.ySnake < 0) {
+      snake.setProps({ y: canvas.height })
+    }
+  }
+
+  /**
+   * @private method for listening for when snake eats food
+   */
+  private foodInteraction() {
+    //create aliases
+    const food = this.instance.food
+    const snake = this.instance.snake
+
+    //update the stage component with current position of the food
+    this.props.xFood = this.instance.food.getProps('x') as number
+    this.props.yFood = this.instance.food.getProps('y') as number
+
+    if (
+      this.props.xFood === this.props.xSnake &&
+      this.props.yFood === this.props.ySnake
+    ) {
+      food.setProps({ empty: true })
+      snake.setProps({ addTail: true })
+    } else {
+      food.setProps({ empty: false })
+      snake.setProps({ addTail: false })
+    }
+  }
+
+  /**
+   * @public starts the game
+   * @param keyboardEvent
+   */
+  public start(param: TInput) {
+    this.input(param.directions)
+    this.boundaryInteraction()
+    this.foodInteraction()
+    this.instance.snake.update()
+    this.instance.food.update()
+  }
+}
