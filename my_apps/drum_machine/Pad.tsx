dc88ea1adad1379c8065de65ada762f9f327f218
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-08-04 04:35:16 +0100

    pad

--- my_apps/drum_machine/Pad.tsx
+++ my_apps/drum_machine/Pad.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
-export const Pad: React.FC<{ parentClass: string; childClass: string }> = ({
-  ...props
-}): JSX.Element => {
-  return (
+type TProps = {
+  parentClass: string
+  childClass: string
+  keyRefClass: string
+  keyRef: string
+}
+
+export const Pad = React.forwardRef<HTMLDivElement, TProps>(
+  (props, forwardedRef) => (
     <>
       <div className={`${props.parentClass} pad`}>
-        <div className={props.childClass} />
+        <div ref={forwardedRef}>
+          <div className={props.keyRefClass}>
+            <div>
+              {props.keyRef}
+            </div>
+          </div>
+          <div className={props.childClass} />
+        </div>
       </div>
     </>
   )
-}
-
+)
 export default Pad
--- my_apps/drum_machine/padControls.ts
+++ my_apps/drum_machine/padControls.ts
@@ -1,16 +1,6 @@
 import { default as $ } from 'jquery'
-// import { Howl } from 'howler'
 
 export default function padControls(params: any) {
-  // // config howler with soundJSON params item
-  // const sound = new Howl({
-  //   src: params.soundsJSON.urls,
-  //   sprite: params.soundsJSON.sprite,
-  //   onend: function () {
-  //     console.log('Finished!')
-  //   },
-  // })
-
   // all pad elements selector
   let pad = $('.pad')
 
@@ -21,6 +11,17 @@ export default function padControls(params: any) {
     params.setState({ padActive: { status: true, index: idx } })
   })
   pad.on('mouseup', function (e) {
+    params.setState({ padActive: { status: false, index: 0 } })
+  })
+
+  // click event
+  pad.on('touchstart', function (e) {
+    params.setState({ padActive: true })
+    const idx = pad.index(this) + 1
+    params.setState({ padActive: { status: true, index: idx } })
+    console.log('touch')
+  })
+  pad.on('touchend', function (e) {
     params.setState({ padActive: { status: false, index: 0 } })
   })
 }
